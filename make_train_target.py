69c71fc80935ee42488226962b29d643d0a6e75a
Administrator <admin@example.com>
2019-10-12 16:31:24 +0200

    fixed bug with all zeros

--- make_train_target.py
+++ make_train_target.py
@@ -11,7 +11,7 @@ class MakeTarget(BaseEstimator, TransformerMixin):
     def __init__(self, start, end, time_col, segment_col, agg_col):
         self.start = start
         self.end = end
-        self.time_col=time_col
+        self.time_col = time_col
         self.segment_col = segment_col
         self.agg_col = agg_col
 
@@ -21,17 +21,17 @@ class MakeTarget(BaseEstimator, TransformerMixin):
     def transform(self, X, y=None):
         train_df = X.set_index(self.time_col)
         train_df = train_df.groupby([pd.Grouper(freq='H'), self.segment_col])[self.agg_col].count()
-        # make into bool
-        train_df = train_df >= 1
         # get daterange
         date_range = pd.date_range(self.start, self.end, freq='H')
         # fill in data
-        out_df = train_df.reset_index().groupby(self.segment_col).apply(reindex, date_range=date_range)
+        out_df = train_df.reset_index().groupby(self.segment_col).apply(self._reindex, date_range=date_range)
         return out_df
 
-
-def reindex(df, date_range):
-    return df.reindex(date_range, fill_value=0)
+    def _reindex(self, df, date_range):
+        df = df.set_index(self.time_col)
+        out_df = df.reindex(date_range, fill_value=0)
+        out_df = out_df[[self.agg_col]]
+        return out_df
 
 
 if __name__ == '__main__':
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     # make col datetime
     train_df['Occurrence Local Date Time'] = pd.to_datetime(train_df['Occurrence Local Date Time'])
     # init transformer
-    make_target = MakeTarget('2016-01-01', '2019-01-01', 'Occurrence Local Date Time',
-                                                                                       'road_segment_id', 'EventId')
+    make_target = MakeTarget('2016-01-01', '2019-01-01', 'Occurrence Local Date Time', 'road_segment_id', 'EventId')
     target = make_target.fit_transform(train_df)
+    print('found events =', target.EventId.sum())
     target.to_csv('train_target.csv')
